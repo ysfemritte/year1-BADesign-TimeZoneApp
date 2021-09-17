23f6c8a03209e29ed32b0e7e4269413bb2585354
Yusuf Emritte <34180742+ysfemritte@users.noreply.github.com>
2021-09-17 17:23:24 +0400

    Add files via upload

---
+++ TimeZone/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TimeZone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cityListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timeZoneLabel = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a city and I will give you a Time Zone:";
+            // 
+            // cityListBox
+            // 
+            this.cityListBox.FormattingEnabled = true;
+            this.cityListBox.ItemHeight = 16;
+            this.cityListBox.Items.AddRange(new object[] {
+            "Denver",
+            "Honolulu",
+            "Minneapolis",
+            "New York",
+            "San Francisco"});
+            this.cityListBox.Location = new System.Drawing.Point(56, 70);
+            this.cityListBox.Name = "cityListBox";
+            this.cityListBox.Size = new System.Drawing.Size(234, 100);
+            this.cityListBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Time Zone:";
+            // 
+            // timeZoneLabel
+            // 
+            this.timeZoneLabel.Location = new System.Drawing.Point(118, 194);
+            this.timeZoneLabel.Name = "timeZoneLabel";
+            this.timeZoneLabel.Size = new System.Drawing.Size(171, 22);
+            this.timeZoneLabel.TabIndex = 3;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(58, 269);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(103, 33);
+            this.okButton.TabIndex = 4;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(186, 269);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(103, 33);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 342);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.timeZoneLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cityListBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox cityListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox timeZoneLabel;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
